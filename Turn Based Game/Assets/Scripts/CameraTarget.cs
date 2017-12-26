3b5ac4372353cad1fde27340d6138fb68f213331
Dwight <dwightmopas@ymail.com>
2017-12-26 02:35:17 -0800

    Setup demo level

---
+++ Turn Based Game/Assets/Scripts/CameraTarget.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTarget : MonoBehaviour {
+
+    public GameObject player;
+
+    void Start()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position;
+    }
+}
